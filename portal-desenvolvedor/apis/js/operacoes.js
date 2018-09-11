a7aaa7e0d66264a621e283e6661a974b0e2f51c0
simei20 <simei.silva@serpro.gov.br>
2018-09-11 11:32:05 -0300

    Update operacoes.js

--- portal-desenvolvedor/apis/js/operacoes.js
+++ portal-desenvolvedor/apis/js/operacoes.js
@@ -51,8 +51,7 @@ function makeOperationBlock(operacao){
     ent += "<h4>Dados de Teste</h4>";
         opCampos = '';
     operacao.teste.forEach(teste => {
-        opCampos += "<tr><td>" + teste.cpf + "</td><td>" + teste.situacao + "</td></tr>";
-        opCampos += "<tr><td>" + teste.cnpj + "</td><td>" + teste.situacao + "</td></tr>";
+        opCampos += "<tr><td>" + teste.ni + "</td><td>" + teste.situacao + "</td></tr>";
     });
     ent += "<table class=\"alt\"><thead><tr><th>NI</th><th>Situação Cadastral</th></tr></thead><tbody>" + opCampos + "</tbody></table>";
     
