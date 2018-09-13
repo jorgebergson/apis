e2db7eb7b0f3b36c77ae0b64fa3f859d936051e3
simei20 <simei.silva@serpro.gov.br>
2018-09-13 14:52:37 -0300

    Update operacoes.js

--- portal-desenvolvedor/apis/js/operacoes.js
+++ portal-desenvolvedor/apis/js/operacoes.js
@@ -27,7 +27,7 @@ function makeOperationBlock(operacao){
     ent += "<div class=\"tab\">";
     ent += "<button class=\"tablinksData"+ operacao.id +" tab\" onclick=\"openTab(event, 'entrada"+ operacao.id +"','Data" + operacao.id + "')\">Entrada</button>";
     ent += "<button class=\"tablinksData"+ operacao.id +" tab\" onclick=\"openTab(event, 'saida"+ operacao.id +"','Data" + operacao.id + "')\">Saída</button>";
-    ent += "<button class=\"tablinksData"+ operacao.id +" tab\" onclick=\"openTab(event, 'teste"+ operacao.id +"','Data" + operacao.id + "')\">Teste</button>";
+    ent += "<button class=\"tablinksData"+ operacao.id +" tab\" onclick=\"openTab(event, 'teste"+ operacao.id +"','Data" + operacao.id + "')\">Dados de Teste</button>";
     ent += "</div>";
     ent += "<div id=\"entrada"+ operacao.id +"\" class=\"tabcontentData"+ operacao.id +" box\" style=\"display:none\">";
     ent += "<h4>Dados de Entrada</h4>";
